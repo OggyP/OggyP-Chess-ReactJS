a9ed6e928c19d665ed4a01e81fc0011df3768da1
Oscar <oggyp@oggyp.com>
2023-04-03 21:08:42 +1000

    formatted

--- src/engine.ts
+++ src/engine.ts
@@ -4,202 +4,202 @@ import { Teams, Vector, PieceCodes } from "./chessLogic/types";
 const debugEngine = false;
 
 class UCIengine {
-  private _engine: Worker;
-  private _isready: boolean;
-  private _analyseFromTeam: Teams = "white";
-  private _infoBuffer: any[] = []
-  multiPV: number;
-  loadedNNUE: boolean = localStorage.getItem('loadNNUE') === 'true'
-  _commandsQueue: string[];
-  constructor(path: string, initConfigCommands: string[] = [], multiPV: number = 1) {
-    this.multiPV = multiPV
-    this._engine = new Worker(path)
-    this._engine.onmessage = (event) => {
-        this.onMessage(event)
-    }
-    this._isready = false;
-    this._commandsQueue = initConfigCommands
-    this._engine.postMessage('uci')
-    this._engine.postMessage('setoption name MultiPV value ' + multiPV)
-    if (this.loadedNNUE) {
-      this.loadNNUE()
-    }
-    window.onbeforeunload = () => { this._engine.postMessage('quit') }
-  }
-
-  go(startingFEN: string, longNotationMoves: string[], type: string) {
-    let startingTeam: Teams = (startingFEN.split(' ')[1] === 'w') ? "white" : "black"
-    if (longNotationMoves.length % 2 === 1) this._analyseFromTeam = (startingTeam === 'white') ? "black" : "white"
-    else this._analyseFromTeam = startingTeam
-    this.addToQueueAndSend('stop')
-    this.addToQueueAndSend('isready')
-    this.addToQueueAndSend(`position fen ${startingFEN} moves ${longNotationMoves.join(' ')}`)
-    this.addToQueueAndSend('go ' + type)
-  }
-
-  reset() {
-    this.addToQueueAndSend('stop')
-    this.addToQueueAndSend('isready')
-    this.addToQueueAndSend('ucinewgame')
-  }
-
-  setDifficulty(skill: number, fastGame: boolean) {
-    this.addToQueueAndSend('stop')
-    this.addToQueueAndSend('isready')
-
-    if (skill !== 20) this.addToQueueAndSend('setoption name Skill Level value ' + skill)
-
-    let engineMoveType = 'movetime 60000'
-
-    if (!fastGame || skill <= 15) {
-      let depth: number | null = null
-      if (skill < 2) {
-        depth = 1;
-      } else if (skill < 5) {
-        depth = 2;
-      } else if (skill < 10) {
-        depth = 3;
-      } else if (skill < 15) {
-        depth = 4;
-      } else {
-        engineMoveType = 'movetime 10000'
-      }
-      if (depth)
-        engineMoveType = 'move depth ' + depth
-    } else
-      engineMoveType = 'movetime 1000'
-    return engineMoveType
-  }
-
-  addToQueueAndSend(cmd: string) {
-    this._commandsQueue.push(cmd)
-    if (this._isready) {
-      const cmdToSend = this._commandsQueue.shift() as string
-      if (debugEngine) console.log("SendC " + cmdToSend)
-      this._engine.postMessage(cmdToSend);
-    }
-    if (['uci', 'isready'].includes(cmd))
-      this._isready = false
-  }
-
-  sendCmd(cmd: string) {
-    if (this._isready) {
-      this._engine.postMessage(cmd);
-      if (debugEngine) console.log("SendD " + cmd)
-      if (['uci', 'isready'].includes(cmd))
-        this._isready = false
-    } else
-      this._commandsQueue.push(cmd)
-  }
-
-  loadNNUE() {
-    this.addToQueueAndSend('stop')
-    this.addToQueueAndSend('isready')
-    this.addToQueueAndSend('setoption name Use NNUE value true')
-    this.loadedNNUE = true
-  }
-
-  onMessage(event: string | { data: string }) {
-    let line: string
-    if (event && typeof event === "object") {
-      line = event.data;
-    } else {
-      line = event;
-    }
-
-    if (debugEngine) console.log(`Receive: ${line}`)
-
-    if (line.startsWith('bestmove')) {
-      if (line === 'bestmove (none)') return
-      const move = line.split(' ')[1]
-      const bestMove: {
-        startingPos: Vector
-        endingPos: Vector
-        promotion?: PieceCodes
-      } = {
-        startingPos: {
-          'x': convertToPosition(move[0], 'x') as number,
-          'y': convertToPosition(move[1], 'y') as number
-        },
-        endingPos: {
-          'x': convertToPosition(move[2], 'x') as number,
-          'y': convertToPosition(move[3], 'y') as number
+    private _engine: Worker;
+    private _isready: boolean;
+    private _analyseFromTeam: Teams = "white";
+    private _infoBuffer: any[] = []
+    multiPV: number;
+    loadedNNUE: boolean = localStorage.getItem('loadNNUE') === 'true'
+    _commandsQueue: string[];
+    constructor(path: string, initConfigCommands: string[] = [], multiPV: number = 1) {
+        this.multiPV = multiPV
+        this._engine = new Worker(path)
+        this._engine.onmessage = (event) => {
+            this.onMessage(event)
         }
-      }
-      if (this._isready) {
-        if (move.length === 5) {
-          bestMove.promotion = move[4] as PieceCodes
+        this._isready = false;
+        this._commandsQueue = initConfigCommands
+        this._engine.postMessage('uci')
+        this._engine.postMessage('setoption name MultiPV value ' + multiPV)
+        if (this.loadedNNUE) {
+            this.loadNNUE()
         }
-        const event = new CustomEvent("bestmove", {
-          detail: bestMove
-        })
-        document.dispatchEvent(event);
-        if (this._infoBuffer.length) {
-          const event = new CustomEvent("engine", {
-            detail: this._infoBuffer
-          })
-          document.dispatchEvent(event);
-          this._infoBuffer = []
-        }
-      }
+        window.onbeforeunload = () => { this._engine.postMessage('quit') }
     }
 
-    if (line.startsWith('info')) {
-      const lineInfo = UCIengine.parseInfoLine(line, this._analyseFromTeam)
-      lineInfo.raw = line
-      if (lineInfo.score) {
-        if (Number(lineInfo.multipv) === this.multiPV || lineInfo.score === 'mate 0') {
-          this._infoBuffer.push(lineInfo)
-          const event = new CustomEvent("engine", {
-            detail: this._infoBuffer
-          })
-          document.dispatchEvent(event);
-          this._infoBuffer = []
-        } else if (Number(lineInfo.multipv) <= this._infoBuffer.length) {
-          const event = new CustomEvent("engine", {
-            detail: this._infoBuffer
-          })
-          document.dispatchEvent(event);
-          this._infoBuffer = [lineInfo]
+    go(startingFEN: string, longNotationMoves: string[], type: string) {
+        let startingTeam: Teams = (startingFEN.split(' ')[1] === 'w') ? "white" : "black"
+        if (longNotationMoves.length % 2 === 1) this._analyseFromTeam = (startingTeam === 'white') ? "black" : "white"
+        else this._analyseFromTeam = startingTeam
+        this.addToQueueAndSend('stop')
+        this.addToQueueAndSend('isready')
+        this.addToQueueAndSend(`position fen ${startingFEN} moves ${longNotationMoves.join(' ')}`)
+        this.addToQueueAndSend('go ' + type)
+    }
+
+    reset() {
+        this.addToQueueAndSend('stop')
+        this.addToQueueAndSend('isready')
+        this.addToQueueAndSend('ucinewgame')
+    }
+
+    setDifficulty(skill: number, fastGame: boolean) {
+        this.addToQueueAndSend('stop')
+        this.addToQueueAndSend('isready')
+
+        if (skill !== 20) this.addToQueueAndSend('setoption name Skill Level value ' + skill)
+
+        let engineMoveType = 'movetime 60000'
+
+        if (!fastGame || skill <= 15) {
+            let depth: number | null = null
+            if (skill < 2) {
+                depth = 1;
+            } else if (skill < 5) {
+                depth = 2;
+            } else if (skill < 10) {
+                depth = 3;
+            } else if (skill < 15) {
+                depth = 4;
+            } else {
+                engineMoveType = 'movetime 10000'
+            }
+            if (depth)
+                engineMoveType = 'move depth ' + depth
         } else
-          this._infoBuffer.push(lineInfo)
-      }
+            engineMoveType = 'movetime 1000'
+        return engineMoveType
     }
 
-    if (['uciok', 'readyok'].includes(line)) {
-      this._infoBuffer = [] // clear info buffer
-      this._isready = true
+    addToQueueAndSend(cmd: string) {
+        this._commandsQueue.push(cmd)
+        if (this._isready) {
+            const cmdToSend = this._commandsQueue.shift() as string
+            if (debugEngine) console.log("SendC " + cmdToSend)
+            this._engine.postMessage(cmdToSend);
+        }
+        if (['uci', 'isready'].includes(cmd))
+            this._isready = false
     }
 
-    while (this._commandsQueue.length > 0 && this._isready) {
-      let popCmd = this._commandsQueue.shift() as string
-      this.sendCmd(popCmd)
+    sendCmd(cmd: string) {
+        if (this._isready) {
+            this._engine.postMessage(cmd);
+            if (debugEngine) console.log("SendD " + cmd)
+            if (['uci', 'isready'].includes(cmd))
+                this._isready = false
+        } else
+            this._commandsQueue.push(cmd)
     }
-  }
 
-  static parseInfoLine(line: string, turn: Teams) {
-    const infoTypes = ['depth', 'seldepth', 'multipv', 'score', 'nodes', 'nps', 'hashfull', 'tbhits', 'time', 'pv', 'string']
-    let info: any = {}
-    let currentInfoType = ''
-    let words = line.split(' ')
-    for (let i = 0; i < words.length; i++) {
-      let word = words[i]
-      if (word === 'info') continue
-      if (infoTypes.includes(word)) {
-        currentInfoType = word
-        continue
-      }
-      if (info.hasOwnProperty(currentInfoType)) {
-        let wordToAdd: string | number = word
-        if (currentInfoType === 'score' && turn === 'black')
-          if (!isNaN(Number(wordToAdd))) wordToAdd = -Number(wordToAdd)
-        info[currentInfoType] += ' ' + wordToAdd
-      } else {
-        info[currentInfoType] = word
-      }
+    loadNNUE() {
+        this.addToQueueAndSend('stop')
+        this.addToQueueAndSend('isready')
+        this.addToQueueAndSend('setoption name Use NNUE value true')
+        this.loadedNNUE = true
     }
-    return info
-  }
+
+    onMessage(event: string | { data: string }) {
+        let line: string
+        if (event && typeof event === "object") {
+            line = event.data;
+        } else {
+            line = event;
+        }
+
+        if (debugEngine) console.log(`Receive: ${line}`)
+
+        if (line.startsWith('bestmove')) {
+            if (line === 'bestmove (none)') return
+            const move = line.split(' ')[1]
+            const bestMove: {
+                startingPos: Vector
+                endingPos: Vector
+                promotion?: PieceCodes
+            } = {
+                startingPos: {
+                    'x': convertToPosition(move[0], 'x') as number,
+                    'y': convertToPosition(move[1], 'y') as number
+                },
+                endingPos: {
+                    'x': convertToPosition(move[2], 'x') as number,
+                    'y': convertToPosition(move[3], 'y') as number
+                }
+            }
+            if (this._isready) {
+                if (move.length === 5) {
+                    bestMove.promotion = move[4] as PieceCodes
+                }
+                const event = new CustomEvent("bestmove", {
+                    detail: bestMove
+                })
+                document.dispatchEvent(event);
+                if (this._infoBuffer.length) {
+                    const event = new CustomEvent("engine", {
+                        detail: this._infoBuffer
+                    })
+                    document.dispatchEvent(event);
+                    this._infoBuffer = []
+                }
+            }
+        }
+
+        if (line.startsWith('info')) {
+            const lineInfo = UCIengine.parseInfoLine(line, this._analyseFromTeam)
+            lineInfo.raw = line
+            if (lineInfo.score) {
+                if (Number(lineInfo.multipv) === this.multiPV || lineInfo.score === 'mate 0') {
+                    this._infoBuffer.push(lineInfo)
+                    const event = new CustomEvent("engine", {
+                        detail: this._infoBuffer
+                    })
+                    document.dispatchEvent(event);
+                    this._infoBuffer = []
+                } else if (Number(lineInfo.multipv) <= this._infoBuffer.length) {
+                    const event = new CustomEvent("engine", {
+                        detail: this._infoBuffer
+                    })
+                    document.dispatchEvent(event);
+                    this._infoBuffer = [lineInfo]
+                } else
+                    this._infoBuffer.push(lineInfo)
+            }
+        }
+
+        if (['uciok', 'readyok'].includes(line)) {
+            this._infoBuffer = [] // clear info buffer
+            this._isready = true
+        }
+
+        while (this._commandsQueue.length > 0 && this._isready) {
+            let popCmd = this._commandsQueue.shift() as string
+            this.sendCmd(popCmd)
+        }
+    }
+
+    static parseInfoLine(line: string, turn: Teams) {
+        const infoTypes = ['depth', 'seldepth', 'multipv', 'score', 'nodes', 'nps', 'hashfull', 'tbhits', 'time', 'pv', 'string']
+        let info: any = {}
+        let currentInfoType = ''
+        let words = line.split(' ')
+        for (let i = 0; i < words.length; i++) {
+            let word = words[i]
+            if (word === 'info') continue
+            if (infoTypes.includes(word)) {
+                currentInfoType = word
+                continue
+            }
+            if (info.hasOwnProperty(currentInfoType)) {
+                let wordToAdd: string | number = word
+                if (currentInfoType === 'score' && turn === 'black')
+                    if (!isNaN(Number(wordToAdd))) wordToAdd = -Number(wordToAdd)
+                info[currentInfoType] += ' ' + wordToAdd
+            } else {
+                info[currentInfoType] = word
+            }
+        }
+        return info
+    }
 }
 
 export default UCIengine
