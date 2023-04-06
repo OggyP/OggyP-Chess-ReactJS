7768c966ef69ba57f1c6617d8564b63ccf31b0fd
Oscar <oggyp@oggyp.com>
2023-04-06 11:09:47 +1000

    update start server

--- startServer.sh
+++ startServer.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-/home/oggyp/.nvm/versions/node/v16.8.0/bin/serve -s -n -l 3003 build --ssl-cert /etc/letsencrypt/live/chess.oggyp.com/cert.pem --ssl-key /etc/letsencrypt/live/chess.oggyp.com/privkey.pem
+/home/oggyp/.nvm/versions/node/v16.8.0/bin/serve -s -n -l 3003 build
