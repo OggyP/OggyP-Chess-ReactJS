5cd1aedabb01fef720b0b90301931bc7e327c9c8
kElnaDev <kelna@kelna.dev>
2024-09-10 09:52:07 +1000

    refactored navbar to make it actually work properly

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,61 +1,60 @@
-import { useState } from 'react';
-import './css/navbar.scss'
+import { PropsWithChildren, useState } from 'react';
+import './css/navbar.scss';
 import GoogleIcon from './tsxAssets/GoogleIcon';
-import { gameModesList } from './helpers/gameModes'
+import { gameModesList } from './helpers/gameModes';
 
-interface NavBarProps {
+
+interface SubmenuProps {
+  name: string,
+  icon?: string,
+  widescreen?: boolean,
+  lastElement?: boolean
 }
 
-function NavBar(props: NavBarProps) {
+
+function Submenu(props: PropsWithChildren<SubmenuProps>) {
   const [open, setOpen] = useState(false);
+  const icon = (props.icon)? <GoogleIcon name={props.icon} /> : props.name;
+  const classOpen = (open)? 'open ' : '';
+  const classWidescreen = (props.widescreen)? 'widescreen-only ' : 'smallscreen-only ';
+  const classLastElement = (props.lastElement)? 'last-element ' : 'not-last-element ';
 
-  const [showGameModes, setShowGameModes] = useState(false);
+  return (
+    <li className={classOpen + classWidescreen + classLastElement + 'submenu'}>
+      <button onClick={() => { setOpen(!open) }}>
+        {icon}
+      </button>
 
+      <nav aria-label={props.name + ' Options'} className='submenu-contents'>
+        <ul>
+          {props.children}
+        </ul>
+      </nav>
+    </li>
+  )
+}
+
+
+function NavBar() {
   return (
     <nav id='navbar' aria-label='Main Menu'>
       <ul>
-        
-        {/* <li className='home'>
-          <a href='/home'>
+        <li className='home'>
+          <a href="/home">
             <img src='/assets/images/logo/white.svg' alt='OggyP Chess logo' />
           </a>
         </li>
 
-        <li className={((open) ? 'submenu open' : 'submenu') + ' widescreen-only'}>
-          <nav aria-label='Analysis Options'>
-            <ul>
-              <li>
-                <div className="dropdown item">
-                  <button className='dropbtn'>Analyse<i className="fa fa-caret-down"></i></button>
-                  <div className="dropdown-content">
-                    {gameModesList.map(value => {
-                      return <a key={value[0]} href={`/analysis/${value[0]}`}>{value[1]}</a>
-                    })}
-                    <a onClick={() => { localStorage.removeItem('token'); window.location.href = '/login' }}>
-                      Logout
-                    </a>
-                  </div>
-                </div>
+        <Submenu name='Analyse' widescreen>
+            {gameModesList.map(value => { return (
+              <li key={value[0]}>
+                <a href={`/analysis/${value[0]}`}>{value[1]}</a>
               </li>
-              <li><a href='/stockfish'>Stockfish</a></li>
-              <li>
-                <button>
-                  <GoogleIcon name='dark_mode' />
-                </button>
-              </li>
-            </ul>
-          </nav>
-        </li>
+            )})}
+        </Submenu>
 
         <li className='widescreen-only'>
-          <button onClick={() => setShowGameModes(!showGameModes)} style={{paddingLeft: "30px", paddingRight: "30px"}}>Analyse</button>
-          {showGameModes && gameModesList.map(value => {
-            return <a className='drop-down' key={value[0]} href={`/analysis/${value[0]}`}>{value[1]}</a>
-          })}
-        </li>
-
-        <li className='widescreen-only'>
-          <a href='/stockfish'>Stockfish</a>
+          <a href="/stockfish">Stockstock</a>
         </li>
 
         <li className='widescreen-only'>
@@ -64,43 +63,43 @@ function NavBar(props: NavBarProps) {
           </button>
         </li>
 
-        <li className='widescreen-only mode-switch'>
+        <li className='first-right-element widescreen-only'>
           <button>
             <GoogleIcon name='dark_mode' />
           </button>
         </li>
 
-        <li className='toggle-button'>
-          <button onClick={() => { setOpen(!open) }}>
-            <img src='/assets/images/hamburger/light.svg' alt='More Options button' />
-          </button>
-        </li>
+        <Submenu name='Main' icon='menu' lastElement>
+          <li>
+            <h3>Analyse</h3>
+          </li>
 
-        <li className={(open) ? 'submenu open' : 'submenu'}>
-          <nav aria-label='Main Options'>
-            <ul>
-              <li>
-                <div className="dropdown item">
-                  <button className='dropbtn'>Analyse<i className="fa fa-caret-down"></i></button>
-                  <div className="dropdown-content">
-                    {gameModesList.map(value => {
-                      return <a key={value[0]} href={`/analysis/${value[0]}`}>{value[1]}</a>
-                    })}
-                    <a onClick={() => { localStorage.removeItem('token'); window.location.href = '/login' }}>
-                      Logout
-                    </a>
-                  </div>
-                </div>
-              </li>
-              <li><a href='/stockfish'>Stockfish</a></li>
-              <li>
-                <button>
-                  <GoogleIcon name='dark_mode' />
-                </button>
-              </li>
-            </ul>
-          </nav>
-        </li> */}
+          {gameModesList.map(value => { return (
+            <li key={'sub-' + value[0]}>
+              <a href={`/analysis/${value[0]}`}>{value[1]}</a>
+            </li>
+          )})}
+
+          <li>
+            <hr />
+          </li>
+
+          <li>
+            <a href="/stockfish">Sockfishh</a>
+          </li>
+
+          <li>
+            <button onClick={() => { localStorage.removeItem('token'); window.location.href = '/login' }}>
+              Logout
+            </button>
+          </li>
+
+          <li>
+            <button>
+              <GoogleIcon name='dark_mode' />
+            </button>
+          </li>
+        </Submenu>
       </ul>
     </nav>
   )
